4f42a0c330f2ead68bd967b3c52ef8e92c757274
Sarah Österhed <sarah.osterhed@gmail.com>
2026-01-06 19:48:16 +0100

    refactor(user-profile): implement ProfileHeader to reuse on profile pages

--- src/pages/Home/UserProfilePage.tsx
+++ src/pages/Home/UserProfilePage.tsx
@@ -3,23 +3,13 @@ import StatusAlert from "@/components/atoms/StatusAlert";
 import { USER_BY_ID } from "@/graphql/queries/users";
 import type { UserData } from "@/graphql/types/user";
 import { useQuery } from "@apollo/client/react";
-import {
-  Heading,
-  VStack,
-  Avatar,
-  Card,
-  HStack,
-  Box,
-  Text,
-  Image,
-} from "@chakra-ui/react";
-import Wiggly from "@/assets/icons/wiggly-line.svg";
+import { Heading, VStack, Card, HStack } from "@chakra-ui/react";
 
 import { useParams } from "react-router-dom";
-import InlineIcon from "@/components/atoms/InlineIcon";
 import formatMessageDate from "../Messages/utils/formatMessageDate";
 import SkillLevelGuide from "@/components/molecules/SkillLevelGuide";
 import HobbyTag from "@/components/molecules/HobbyTag";
+import ProfileHeader from "@/components/organisms/ProfileHeader/ProfileHeader";
 
 const UserProfilePage = () => {
   const { userId } = useParams();
@@ -58,48 +48,13 @@ const UserProfilePage = () => {
 
   return (
     <VStack gap={6}>
-      <VStack
-        bg="neutral.900"
-        width="full"
-        gap={4}
-        py={6}
-        px={4}
-        borderRadius={1}
-      >
-        <Box asChild width={40}>
-          <Avatar.Root size="full">
-            <Avatar.Fallback name={userName} />
-            <Avatar.Image
-              src={profileImageUrl ?? undefined}
-              alt={`Profile image of ${userName}`}
-            />
-          </Avatar.Root>
-        </Box>
-        <VStack gap={2}>
-          <Heading as="h1" size="2xl">
-            {userName}
-          </Heading>
-          <HStack>
-            <Image src={Wiggly} alt="Decorative wiggly line" />
-            {/* TODO: Add translation */}
-            <Text fontSize="lg" lineHeight={1.25} fontWeight="600">
-              {age} år
-            </Text>
-            <Image src={Wiggly} alt="Decorative wiggly line" />
-          </HStack>
-        </VStack>
-        <HStack gap={6}>
-          <HStack gap={0.5}>
-            <InlineIcon name="distance" color="green.200" fontSize="1.5rem" />
-            <Text fontSize="sm">{location}</Text>
-          </HStack>
-          <HStack gap={1}>
-            <InlineIcon name="verified" color="yellow.100" fontSize="1.5rem" />
-            {/* TODO: Add translation */}
-            <Text fontSize="sm">Medlem sedan: {memberSince}</Text>
-          </HStack>
-        </HStack>
-      </VStack>
+      <ProfileHeader
+        name={userName}
+        age={age}
+        location={location}
+        memberSince={memberSince}
+        imageUrl={profileImageUrl}
+      />
 
       <VStack width="full" alignItems="start" gap={2}>
         {/* TODO: Add translation */}
@@ -123,7 +78,7 @@ const UserProfilePage = () => {
           flex="1"
           bg="neutral.900"
           variant="subtle"
-          borderRadius={1}
+          borderRadius={4}
         >
           <Card.Body p={4} pb={6}>
             <Card.Description color="neutral.100" fontSize="md">
